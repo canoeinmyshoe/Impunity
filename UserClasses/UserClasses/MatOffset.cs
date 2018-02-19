c08d17d4cc242f761bdda695990d9e7fb8bfc8e0
Timothy Floyd <timTheNetDev@gmail.com>
2018-02-19 15:43:11 -0800

    Experimented with impunity classes.

---
+++ UserClasses/UserClasses/MatOffset.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ImpunityEngine;
+
+namespace UserClasses
+{
+    public class MatOffset : ImpunityClass
+    {
+
+        public override void Update(SceneObject sceneObject)
+        {
+            float x = sceneObject.material.xOffset;
+            float y = sceneObject.material.yOffset;
+            sceneObject.SetMaterialOffset(x + 0.001f, y);
+        }
+    }
+}
